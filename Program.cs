3704a675887acb731906753a9c571db1ff262b14
Gaetano <gaem195@gmail.com>
2024-01-29 16:21:43 +0100

    add various classes

--- Program.cs
+++ Program.cs
@@ -4,11 +4,14 @@
     {
         static void Main(string[] args)
         {
-            Atleta bestAthete = new Atleta("Marco Silveri", 324);
-            Console.WriteLine($"{bestAthete.Name} is our fastest athlete, he's only {bestAthete.Age} old!");
+            Atleta bestAthlete = new Atleta("Marco", 323);
+
+
+            Console.WriteLine($"{bestAthlete.Name} is our fastest athlete, he's only {bestAthlete.Age} old!");
             Console.WriteLine();
 
             Dipendente superDipendente = new Dipendente("Alessandro", 32, "Microsoft");
+            superDipendente.Age = 0;
             Console.WriteLine($"{superDipendente.Name}, di anni {superDipendente.Age}, ha appena ricevuto una promozione presso la compagnia {superDipendente.Company}");
             Console.WriteLine();
 
@@ -18,131 +21,189 @@
             Console.WriteLine($"Ma una fa {Animale.Verso("moo")} {Animale.Verso("moooo")}!!!!!!");
             Console.WriteLine();
 
+            Veicolo nissanGT = new Veicolo("Nissan", "GT-R", 190, 230);
+
+            nissanGT.CaratteristicheVeicolo();
+            Console.WriteLine();
+
+            string provaMetodo = "Questa auto è velocissima! " + nissanGT.MassimaVelocita();
+            Console.WriteLine(provaMetodo);
+
+
             Console.ReadKey();
         }
     }
-}
 
 
 
-public class Atleta
-{
-    private string name;
-    private int age;
-    public string Name
+
+    public class Atleta
     {
-        get { return name; }
-        set { name = value; }
-    }
+        private string name;
+        private int age;
 
-    public int Age
-    {
-        get { return age; }
-        set
+        public string Name
         {
-            if (age <= 0)
+            get { return name; }
+            set { name = value; }
+        }
+
+        public int Age
+        {
+            get { return age; }
+            set
             {
-                age = 1;
+                if (age == 0)
+                {
+                    age = 30;
+
+                }
+                else
+                {
+                    age = value;
+                }
             }
-            else
-            {
-                age = value;
-            }
+        }
+
+        public Atleta(string name, int age)
+        {
+            this.name = name;
+            this.age = age;
+
         }
     }
 
-    public Atleta(string name, int age)
+    public class Dipendente
     {
-        this.name = name;
-        this.age = age;
-
-    }
-}
-
-public class Dipendente
-{
-    private string name;
-    private int age;
-    private string company;
-    public string Name
-    {
-        get { return name; }
-        set { name = value; }
-    }
-
-    public int Age
-    {
-        get { return age; }
-        set
+        private string name;
+        private int age;
+        private string company;
+        public string Name
         {
-            if (age <= 0)
+            get { return name; }
+            set { name = value; }
+        }
+
+        public int Age
+        {
+            get { return age; }
+            set
             {
-                age = 1;
+                if (age <= 0)
+                {
+                    age = 1;
+                }
+                else
+                {
+                    age = value;
+                }
             }
-            else
-            {
-                age = value;
-            }
+        }
+
+
+        public string Company
+        {
+            get { return company; }
+            set { company = value; }
+        }
+
+        public Dipendente(string name, int age, string company)
+        {
+            this.name = name;
+            this.age = age;
+            this.company = company;
+
         }
     }
 
-
-    public string Company
+    public class Animale
     {
-        get { return company; }
-        set { company = value; }
-    }
+        private string name;
 
-    public Dipendente(string name, int age, string company)
-    {
-        this.name = name;
-        this.age = age;
-        this.company = company;
-
-    }
-}
-
-public class Animale
-{
-    private string name;
-
-    public string Name
-    {
-        get { return name; }
-        set { name = value; }
-    }
-
-    public Animale(string name)
-    {
-        this.name = name;
-
-
-    }
-
-    public static string Verso(string verso)
-    {
-        return verso.ToUpper();
-    }
-
-}
-
-public class Veicolo
-{
-    private string brand;
-    private string model;
-    private int horsePower;
-    private int topSpeed;
-
-    public string Brand
-    {
-        get { return brand; }
-        set
+        public string Name
         {
-            if (brand == "") { brand = "missing brand"}
-            else
+            get { return name; }
+            set { name = value; }
+        }
+
+        public Animale(string name)
+        {
+            this.name = name;
+
+
+        }
+
+        public static string Verso(string verso)
+        {
+            return verso.ToUpper();
+        }
+
+    }
+
+    public class Veicolo
+    {
+        private string brand;
+        private string model;
+        private int horsePower;
+        private int topSpeed;
+
+        public Veicolo(string brand, string model, int horsePower, int topSpeed)
+        {
+            this.brand = brand;
+            this.model = model;
+            this.horsePower = horsePower;
+            this.topSpeed = topSpeed;
+        }
+
+        public string Brand
+        {
+            get { return brand; }
+            set
             {
-                brand = value;
+                if (brand == "") { brand = "missing brand"; }
+                else
+                {
+                    brand = value;
+                }
             }
         }
+        public string Model
+        {
+            get { return model.ToUpper(); }
+            set
+            {
+                if (model == "") { model = "Generic"; }
+                else
+                {
+                    model = value;
+                }
+            }
+        }
+
+        public string HorsePower
+        {
+            get { return horsePower + "CV"; }
+        }
+
+        public string TopSpeed
+        {
+            get { return topSpeed + "Km/h"; }
+        }
+
+        public string MassimaVelocita()
+        {
+            return "LA MASSIMA VELOCITA' E' " + TopSpeed;
+        }
+
+        public void CaratteristicheVeicolo()
+        {
+            Console.WriteLine($"Ecco l'auto selezionata:");
+            Console.WriteLine($"Brand: {Brand}");
+            Console.WriteLine($"Model: {Model}");
+            Console.WriteLine($"Horse Power: {HorsePower}");
+            Console.WriteLine($"Top Speed: {TopSpeed}");
+        }
+
     }
+
 }
